176f5cc9456e31b5b5472e055835f5aee10afac9
Opu Hasnat <opqclick@gmail.com>
2022-06-13 17:32:43 +0600

    Genrating list and list methods

---
+++ list.py
@@ -0,0 +1,20 @@
+l = []
+
+num_1 = 1
+
+while num_1 <= 100:
+    l.append(num_1)
+    num_1 += 1
+print(l);
+
+ll = []
+for num_2 in range(100):
+    ll.append(num_2)
+
+print(ll);
+
+lll = []
+for num_3 in range(100):
+    lll.append(num_3+1)
+
+print(lll);
